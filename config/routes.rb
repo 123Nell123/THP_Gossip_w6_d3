35c943806e8beb7aac27073f9d6ed4843bce3021
123Nell123 <nellp12321@gmail.com>
2022-02-15 02:06:42 -0800

    affichage id by url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 #affichage GET par défaut avec home methode ET page !!!
-  root to:'static_pages_controller#home'
+  root to: 'static_pages#home'
 
   #page statiques
   get 'static_pages/team'
@@ -10,11 +10,11 @@ Rails.application.routes.draw do
  
 
 #route dynamique
-  get 'static_pages(/:var)' , to: 'static_pages#firstname', as: 'welcome' #création d'un nom pour le chemin
+  get 'firstname(/:var)' , to: 'static_pages#firstname', as: 'welcome' #création d'un nom pour le chemin
 
 
 #route dynamique affichage GET nouveau gossip
-get 'gossip(/:id)', to: 'static_pages#gossip
+get 'gossip(/:id)', to: 'static_pages#gossip'
 
 end
  
